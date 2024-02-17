e59f2535c1a5814c34442ef5b35f14251438ac0d
princerodrick59 <116687818+princerodrick59@users.noreply.github.com>
2024-02-17 13:53:14 -0600

    commit configs

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024-Crescendo-AdvantageKit";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 12;
-  public static final String GIT_SHA = "fbc0a344b61803bd1445554942987a35a4f32d77";
-  public static final String GIT_DATE = "2024-02-15 19:12:33 PST";
+  public static final int GIT_REVISION = 13;
+  public static final String GIT_SHA = "77890008decc2ea5238dd9af840d6e104c5df45f";
+  public static final String GIT_DATE = "2024-02-16 19:53:25 PST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-02-16 19:44:27 PST";
-  public static final long BUILD_UNIX_TIME = 1708141467191L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2024-02-17 11:50:16 PST";
+  public static final long BUILD_UNIX_TIME = 1708199416758L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
